b644824a4b92dd0f51fe40ccf70f220aaea47dd9
Vicentendistes <112288602+Vicentendistes@users.noreply.github.com>
2023-11-09 17:33:38 -0300

    OCR added

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from app import app, DEBUG
-from utils import classify
+from utils import classify, OCR
 from flask import Flask, jsonify, request
 
 #   En servidor:5001/summ esta app recibe los POST con los textos para procesar
 @app.route('/summ', methods=['POST'])
 def submit():
     #   request contiene la informacion entregada en la request
-    file = request.files['texto']
+    file = request.files['file']
     #   en este caso request.files['texto'] contiene el texto
-    text =  (file.read()).decode()
+    img_bytes = file.read()
+    text = OCR(img_bytes)
     #   se usa la funcion summarize definida en utils
     if DEBUG:
-        print(text)
+        print(file)
     result = classify(text)
     return result
 
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@
 from app import model, tokenizer, device
 import io
 import torch
+import cv2
+import pytesseract
 
 DEBUG = False
 
@@ -18,3 +20,31 @@ def classify(text):
     predicted_class_id = logits.argmax().item()
     out = model.config.id2label[predicted_class_id]
     return out
+
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def thresholding_invert(image):
+  image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+  # Contar píxeles negros y blancos
+  total_pixels = image.size
+  black_pixels = np.count_nonzero(image == 0)
+  white_pixels = total_pixels - black_pixels
+
+  # Determinar si hay más píxeles negros que blancos
+  if black_pixels > white_pixels:
+      # Invertir colores (negro a blanco, blanco a negro)
+      image = cv2.bitwise_not(image)
+      
+  return image
+
+
+
+def OCR(img_bytes):
+    img = cv2.imread(img_bytes)
+    gray = get_grayscale(img)
+    thres = thresholding_invert(gray)
+    data = pytesseract.image_to_strig(thres, lang="spa")
+    return data
