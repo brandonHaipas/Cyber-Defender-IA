a8660d49e09abb3ba5a311a697453ba13d7bf4f1
Brandon Peña <brandon.pena96@gmail.com>
2023-11-11 03:56:38 -0300

    ia API working, gitignore actualized

---
+++ app-ia/main.py
@@ -0,0 +1,19 @@
+from app import app, DEBUG
+from utils import classify, OCR
+from flask import Flask, jsonify, request
+
+#   En servidor:5001/classify esta app recibe los POST con los textos para procesar
+@app.route('/classify', methods=['POST'])
+def submit():
+    #   request contiene la informacion entregada en la request
+    file = request.files['file']
+    #   en este caso request.files['texto'] contiene el texto
+    text = OCR(file)
+    #   se usa la funcion summarize definida en utils
+    if DEBUG:
+        print(file)
+    result = classify(text)
+    return jsonify({'class_name': result})
+
+if __name__ == "__main__":
+    app.run(port=5001)
---
+++ app-ia/utils.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*- 
+from app import model, tokenizer, device
+import io
+import torch
+import cv2
+import pytesseract
+import numpy as np
+
+DEBUG = False
+#for windows
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
+def classify(text):
+    if DEBUG:
+        print('/////////////////////////////////')
+        print(text)
+        print('/////////////////////////////////')
+
+    #   tokenizer recibe el texto (String) y entrega inputs_ids y attention_mask, que son Tensor, el tipo que usa Pytorch para los vectores / tensores
+    input = tokenizer(text, return_tensors="pt").to(device)
+    with torch.no_grad():
+        logits= model(**input).logits
+    predicted_class_id = logits.argmax().item()
+    out = model.config.id2label[predicted_class_id]
+    return out
+
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def thresholding_invert(image):
+  image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+  # Contar píxeles negros y blancos
+  total_pixels = image.size
+  black_pixels = np.count_nonzero(image == 0)
+  white_pixels = total_pixels - black_pixels
+
+  # Determinar si hay más píxeles negros que blancos
+  if black_pixels > white_pixels:
+      # Invertir colores (negro a blanco, blanco a negro)
+      image = cv2.bitwise_not(image)
+      
+  return image
+
+
+
+def OCR(img_bytes):
+    b_img = img_bytes.read()
+    img = np.asarray(bytearray(b_img))
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    gray = get_grayscale(img)
+    thres = thresholding_invert(gray)
+    data = pytesseract.image_to_string(thres, lang="spa")
+    return data
